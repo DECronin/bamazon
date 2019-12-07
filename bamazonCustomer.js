a13dba3595a2c2d209a1e8c5fe3b6b3cce462643
Dana Cronin <cronin43224235@gmail.com>
2019-12-07 16:51:13 -0500

    fixing name and starting connections

---
+++ bamazonCustomer.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql');
+var inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon"
+})
